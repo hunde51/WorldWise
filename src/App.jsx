3dcb102414a742e89b6e9b221a88788a4322d543
hunde51 <hundetesfa6@gmail.com>
2025-11-25 00:31:16 +0300

    Initial static part — added components and stylin

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -7,7 +9,10 @@ import Login from "./pages/Login";
 import PageNOtFound from "./pages/PageNotFound";
 import "./index.css";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+
 const BASE_URL = "http://localhost:8000";
 function App() {
   const [cities, setCities] = useState([]);
@@ -27,24 +32,32 @@ function App() {
     }
     fetchCities();
   }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+
         <Route path="pricing" element={<Pricing />} />
+
         <Route path="product" element={<Product />} />
+
         <Route path="/login" element={<Login />} />
+
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
+
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Country</p>} />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path={"cities/:id"} element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNOtFound />} />
       </Routes>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,19 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const navigate = useNavigate();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>map</h1>
+      <h1>
+        Position: {lat},{lng}
+      </h1>
+    </div>
+  );
 }
 
 export default Map;
